064714c169131ca08b73ae7aed2e580311722a38
MonicaDJohnson <johnsondmonica290@gmail.com>
2018-05-25 15:46:41 -0400

    random numbers generated

--- Lucky Numbers Project/Program.cs
+++ Lucky Numbers Project/Program.cs
@@ -10,6 +10,122 @@ namespace Lucky_Numbers_Project
     {
         static void Main(string[] args)
         {
+            int numRangeMin;
+            int numRangeMax;
+            int[] userChoices = new int[6];
+            int[] randomNumbers = new int[6];
+            int jackpot = 24000000;
+            int numbersCorrect;
+            int userWinnings;
+            int userGuesses;
+            
+
+            Console.WriteLine("Welcome to the Lucky Numbers Lottery Game.You can win a jackpot of up to $24000000. Please choose your starting number.");
+            numRangeMin = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please choose your ending number.");
+            numRangeMax = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < userChoices.Length; i++)
+            {
+                Console.WriteLine("Please choose six numbers from the number range above.");
+                userGuesses = int.Parse(Console.ReadLine());
+
+                userChoices[i] = userGuesses;
+
+                
+            }
+
+            Random r = new Random();
+            for (int i = 0; i < randomNumbers.Length; i++)
+            {
+
+                randomNumbers[i] = r.Next(numRangeMin, numRangeMax + 1);
+                Console.WriteLine("Lucky Number: " + randomNumbers[i]);
+
+            }
+
+
+
+
+
+
+
         }
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
     }
 }
