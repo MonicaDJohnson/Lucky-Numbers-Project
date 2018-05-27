f52ef33b371918d497dacdc0d5a5981f4c25d430
MonicaDJohnson <johnsondmonica290@gmail.com>
2018-05-27 19:37:02 -0400

    Final

--- Lucky Numbers Project/Program.cs
+++ Lucky Numbers Project/Program.cs
@@ -10,77 +10,100 @@ namespace Lucky_Numbers_Project
     {
         static void Main(string[] args)
         {
-            int numRangeMin;
-            int numRangeMax;
-            int[] userChoices = new int[6];
-            int[] randomNumbers = new int[6];
-            int jackpot = 60000;
-            int numbersCorrect = 0;
-            int userWinnings;
-            int userGuesses;
-            
+            string answerExit = "";
 
-            Console.WriteLine("Welcome to the Lucky Numbers Lottery Game. You can win a jackpot of up to $60000. Please choose your starting number.");
-            numRangeMin = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Please choose your ending number.");
-            numRangeMax = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < userChoices.Length; i++)
+            do
             {
-                Console.WriteLine("Please choose six numbers from the number range above.");
-                userGuesses = int.Parse(Console.ReadLine());
-                while(userGuesses < numRangeMin || userGuesses > numRangeMax)
-                {
-                    Console.WriteLine("The number you have chosen is outside the range of numbers chosen. Please choose numbers within range.");
-                    userGuesses = int.Parse(Console.ReadLine());
-                }
+                int numRangeMin;
+                int numRangeMax;
+                int[] userChoices = new int[6];
+                int[] randomNumbers = new int[6];
+                int jackpot = 60000;
+                int numbersCorrect = 0;
+                int userWinnings;
+                int userGuesses;
 
-                while(userChoices.Contains(userGuesses))
+
+                Console.WriteLine("Welcome to the Lucky Numbers Lottery Game. You can win a jackpot of up to $60000. Please choose your starting number.");
+                numRangeMin = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please choose your ending number.");
+                numRangeMax = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i < userChoices.Length; i++)
                 {
-                    if (true)
+                    Console.WriteLine("Please choose six numbers from the number range above.");
+                    userGuesses = int.Parse(Console.ReadLine());
+                    while (userGuesses < numRangeMin || userGuesses > numRangeMax)
                     {
-                        Console.WriteLine("You have already entered this number. Please select a different number.");
+                        Console.WriteLine("The number you have chosen is outside the range of numbers chosen. Please choose numbers within range.");
                         userGuesses = int.Parse(Console.ReadLine());
                     }
 
-                    else break;
-                }
-                userChoices[i] = userGuesses;
-
-                
-            }
-
-            Random r = new Random();
-            for (int i = 0; i < randomNumbers.Length; i++)
-            {
-
-                randomNumbers[i] = r.Next(numRangeMin, numRangeMax + 1);
-                Console.WriteLine("Lucky Number: " + randomNumbers[i]);
-
-            }
-
-            for (int i =0; i < userChoices.Length; i++)
-            {
-                for (int j = 0; j < randomNumbers.Length; j++)
-                {
-                    if (userChoices[i] == randomNumbers[j])
+                    while (userChoices.Contains(userGuesses))
                     {
-                        numbersCorrect += 1;
+                        if (true)
+                        {
+                            Console.WriteLine("You have already entered this number. Please select a different number.");
+                            userGuesses = int.Parse(Console.ReadLine());
+                        }
+
+                        else break;
+                    }
+                    userChoices[i] = userGuesses;
+
+
+                }
+
+                Random r = new Random();
+                for (int i = 0; i < randomNumbers.Length; i++)
+                {
+
+                    randomNumbers[i] = r.Next(numRangeMin, numRangeMax + 1);
+                    Console.WriteLine("Lucky Number: " + randomNumbers[i]);
+
+                }
+
+                for (int i = 0; i < userChoices.Length; i++)
+                {
+                    for (int j = 0; j < randomNumbers.Length; j++)
+                    {
+                        if (userChoices[i] == randomNumbers[j])
+                        {
+                            numbersCorrect += 1;
+                        }
+
                     }
 
                 }
 
+                Console.WriteLine("You have chosen " + numbersCorrect + " numbers correctly. ");
+                userWinnings = numbersCorrect * 10000;
+                Console.WriteLine("Congratulations! You have won $ " + userWinnings + " dollars!!");
+                Console.WriteLine("Would you like to play again?");
+                answerExit = Console.ReadLine().ToLower();
+                if (answerExit == "no")
+                {
+                    Console.WriteLine("Thanks for playing!");
+                    break;
+                   
+                }
+                
+            }
+            while (answerExit == "yes");
+                  
+
+                
+
+
+
+
+
             }
 
-            Console.WriteLine("You have chosen " + numbersCorrect + " numbers correctly. ");
-            userWinnings = numbersCorrect * 10000;
-            Console.WriteLine("Congratulations! You have won $ " + userWinnings + " dollars!!");
 
-
-                
-                
-        }
+        } 
+    }
 
 
     
@@ -156,5 +179,5 @@ namespace Lucky_Numbers_Project
 
 
       
-    }
-}
+    
+
